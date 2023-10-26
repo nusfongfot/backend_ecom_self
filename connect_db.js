78b60a7525d1f2d92af45fc238fbc1e20395037b
Sorawit <bonusza456@gmail.com>
2023-10-27 01:59:06 +0700

    e

--- connect_db.js
+++ connect_db.js
@@ -1,8 +1,11 @@
 const mysql = require("mysql2");
+const host = "0.0.0.0";
 const client = mysql.createConnection({
-  host: "localhost",
+  host: host || "localhost",
   user: "root",
   database: "nus",
 });
+
+// const client = mysql.createConnection(process.env.DATABASE_URL)
 
 module.exports = client;
