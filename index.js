2404bb411862afd1d3fff55ebeea884f3e8b316b
Sorawit <bonusza456@gmail.com>
2023-10-27 20:08:45 +0700

    ed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3056;
 const host = "0.0.0.0";
 require("dotenv").config();
 
